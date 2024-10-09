5925d2c27fe566311f22df562356934b3812f55b
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-10-09 15:43:49 -0600

    Agregar script de ejecucion maestro

---
+++ Scripts/000-SICBA-EjecutorMaestro-05102024.sql
@@ -0,0 +1,16 @@
+/****************************************************************************************
+* IMASD S.A. DE C.V
+=========================================================================================
+* Descripción: Ejecutor Maestro de la base de datos de Control de Bienes
+*
+* Historial de cambios:
+* ---------------------------------------------------------------------------------------
+* Revisión | Fecha     | Desarrollador | Resumen del cambio
+* ---------------------------------------------------------------------------------------
+*     1    | 05/10/2024| Ramiro May Moo | Creación
+*     1    | 09/10/2024| Ramiro May Moo | Creaacion del script de carga de datos de los
+           |           |                | modulos delsistema.
+****************************************************************************************/
+
+:r 001-SICBA-CrearBaseDatos-17072024.sql
+:r 002-SICBA-CargarDatosModulosSistema-09102024.sql
