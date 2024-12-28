a7d66b7d6e2c7d88d83276107232be42fc8e14d4
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-28 12:37:57 -0600

    Agregar nuevos scripts sql para carga de datos y catálogos

--- Scripts/000-SICBA-EjecutorMaestro-05102024.sql
+++ Scripts/000-SICBA-EjecutorMaestro-05102024.sql
@@ -8,11 +8,29 @@
 * Revisión | Fecha     | Desarrollador | Resumen del cambio
 * ---------------------------------------------------------------------------------------
 *     1    | 05/10/2024| Ramiro May Moo | Creación.
-*     1    | 09/10/2024| Ramiro May Moo | Se agrego el script de carga de datos de los
+*     2    | 09/10/2024| Ramiro May Moo | Se agrego el script de carga de datos de los
 *          |           |                | modulos delsistema.
-*     1    | 10/09/2024| Ramiro May Moo | Se agrego el scripts que crean los triggers.
+*     3    | 10/09/2024| Ramiro May Moo | Se agrego el scripts que crean los triggers.
+*     4    | 06/11/2024| Ramiro May Moo | Se agrego el script que carga los datos de los
+*          |           |                | catalogos.
+*     5    | 01/12/2024| Ramiro May Moo | Se agrego el script que carga los datos de los
+*          |           |                | generales.
+*     6    | 01/12/2024| Ramiro May Moo | Se agrego el script que carga los datos de los
+*          |           |                | patrimonio.
+*     7    | 09/12/2024| Ramiro May Moo | Se agrego el script que carga los datos de los
+*          |           |                | BMS.
 ****************************************************************************************/
 
 :r 001-SICBA-CrearBaseDatos-17072024.sql
 :r 002-SICBA-CrearTriggers-17102024.sql
 :r 003-SICBA-CargarDatosModulosSistema-09102024.sql
+:r 004-SICBA-CargaDatosCatalogos-06112024.sql
+:r 005-SICBA-CargaDatosCatalogosCaracteristicasBienes-06112024.sql
+:r 006-SICBA-CargaDatosCatalogosUbicaciones-06112024.sql
+:r 007-SICBA-CargaDatosCatalogosMarcas-06112024.sql
+:r 008-SICBA-CargaDatosCatalogosLineasVehiculares-06112024.sql
+:r 009-SICBA-CargaDatosCatalogosVersionesVehiculares-06112024.sql
+:r 010-SICBA-CargaDatosGenerales-01122024.sql
+:r 011-SICBA-CargaDatosPatrimonio-01122024.sql
+:r 012-SICBA-CargaDatosBMS-09122024.sql
+
