875f258016b3f143b7b235ee926d5a856c73d738
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 17:48:07 -0600

    Agrega implementacion de funcionalidades de recuperacion de datos del usuario por token

---
+++ ControlBienes.Business/Features/Seguridad/IdentityAccess/BusIdentityAccess.cs
@@ -0,0 +1,39 @@
+﻿using ControlBienes.Business.Contrats.Seguridad;
+using ControlBienes.Business.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Seguridad.IdentityAccess
+{
+	public class BusIdentityAccess : IBusIdentityAccess
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+
+		public BusIdentityAccess(IHttpContextAccessor httpContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+		}
+		public long BObtenerIdUsuario()
+		{
+			var idUsuario = _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+			return long.Parse(idUsuario);
+		}
+
+		public long BObtenerIdUsuario(string token)
+		{
+			var handler = new JwtSecurityTokenHandler();
+			var jwtToken = handler.ReadJwtToken(token);
+			var idUsuario = jwtToken?.Claims
+				.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+			return long.Parse(idUsuario);
+		}
+
+	}
+}
