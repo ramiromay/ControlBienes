74220236dcf1c057042fa369d419b0a6f7c980d1
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 17:12:12 -0600

    Agrega implementacion de funcionalidades de lectura para nacionalidades

---
+++ ControlBienes.Business/Features/General/Nacionalidad/BusNacionalidad.cs
@@ -0,0 +1,55 @@
+﻿using ControlBienes.Business.Contrats.General;
+using ControlBienes.Business.Exceptions;
+using ControlBienes.Business.Genericos;
+using ControlBienes.Data.Contrats.General;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.Constants;
+using ControlBienes.Entities.General.Nacionalidad;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.General.Nacionalidad
+{
+	public class BusNacionalidad : IBusNacionalidad
+	{
+		private readonly IDatNacionalidad _repositorio;
+		private readonly ILogger<BusNacionalidad> _logger;
+		const EnumCodigoOperacion _code = EnumCodigoOperacion.CodeOkNacionalidad;
+		const EnumCodigoOperacion _codeError = EnumCodigoOperacion.CodeErrorNacionalidad;
+
+		public BusNacionalidad(IDatNacionalidad repositorio, ILogger<BusNacionalidad> logger)
+		{
+			_repositorio = repositorio;
+			_logger = logger;
+		}
+		public async Task<EntityResponse<IEnumerable<EntNacionalidadResponse>>> BObtenerTodadNacionalidades()
+		{
+			var nombreMetodo = nameof(BObtenerTodadNacionalidades);
+			var resultado = new EntityResponse<IEnumerable<EntNacionalidadResponse>>();
+
+			_logger.LogInformation($"{(long)_code}: Inicia la operacion para consultar todos las nacionalidades");
+			try
+			{
+				var dto = await _repositorio.DObtenerProyeccionListaAsync<EntNacionalidadResponse>();
+				resultado.Success(dto, _code);
+			}
+			catch (Exception ex)
+			{
+				resultado = BusExceptionHandler.Handle<IEnumerable<EntNacionalidadResponse>>(
+					_logger,
+					ex,
+					_codeError,
+					nombreMetodo,
+					"Ocurrio un error al intentar consultar todos las nacionalidades"
+				);
+			}
+
+			return resultado;
+		}
+	}
+}
---
+++ ControlBienes.Business/Features/General/Nacionalidad/BusNacionalidadMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ControlBienes.Entities.General.Nacionalidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.General.Nacionalidad
+{
+	public class BusNacionalidadMapping : Profile
+	{
+		public BusNacionalidadMapping()
+		{
+			CreateMap<EntNacionalidad, EntNacionalidadResponse>()
+				.ForMember(dest => dest.IdNacionalidad, opt => opt.MapFrom(src => src.iIdNacionalidad))
+				.ForMember(dest => dest.Nombre, opt => opt.MapFrom(src => src.sNombre));
+		}
+	}
+}
