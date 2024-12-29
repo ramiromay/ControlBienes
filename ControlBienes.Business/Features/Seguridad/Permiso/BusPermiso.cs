e7cbf6b2a8d9bf8ecb7fdc36d927ba7c5df9d646
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 17:47:20 -0600

    Agrega implementacion de funcionalidades de lectura para el listado de permisos

---
+++ ControlBienes.Business/Features/Seguridad/Permiso/BusPermiso.cs
@@ -0,0 +1,57 @@
+﻿using ControlBienes.Business.Contrats.General;
+using ControlBienes.Business.Contrats.Seguridad;
+using ControlBienes.Business.Exceptions;
+using ControlBienes.Business.Genericos;
+using ControlBienes.Data.Contrats.Seguridad;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.Constants;
+using ControlBienes.Entities.Seguridad.Permiso;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Seguridad.Permiso
+{
+	public class BusPermiso : IBusPermiso
+	{
+		private readonly IDatPermiso _repositorio;
+		private readonly ILogger<BusPermiso> _logger;
+		const EnumCodigoOperacion _code = EnumCodigoOperacion.CodeOkPermiso;
+		const EnumCodigoOperacion _codeError = EnumCodigoOperacion.CodeErrorPermiso;
+
+		public BusPermiso(IDatPermiso repositorio, ILogger<BusPermiso> logger)
+		{
+			_repositorio = repositorio;
+			_logger = logger;
+		}
+
+		public async Task<EntityResponse<IEnumerable<EntPermisoResponse>>> BObtenerTodosPermisoAsync()
+		{
+			var nombreMetodo = nameof(BObtenerTodosPermisoAsync);
+			var resultado = new EntityResponse<IEnumerable<EntPermisoResponse>>();
+
+			_logger.LogInformation($"{(long)_code}: Inicia la operacion para consultar todos los permisos");
+			try
+			{
+				var resultDto = await _repositorio.DObtenerProyeccionListaAsync<EntPermisoResponse>();
+				resultado.Success(resultDto, _code);
+			}
+			catch (Exception ex)
+			{
+				resultado = BusExceptionHandler.Handle<IEnumerable<EntPermisoResponse>>(
+					_logger,
+					ex,
+					_codeError,
+					nombreMetodo,
+					"Ocurrio un error al intentar consultar todos los permisos"
+				);
+			}
+
+			return resultado;
+		}
+	}
+}
---
+++ ControlBienes.Business/Features/Seguridad/Permiso/BusPermisoMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.General.Periodo;
+using ControlBienes.Entities.Seguridad.Permiso;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Seguridad.Permiso
+{
+	internal class BusPermisoMapping : Profile
+	{
+		public BusPermisoMapping()
+		{
+			CreateMap<EntPermiso, EntPermisoResponse>()
+				.ForMember(dest => dest.IdPermiso, opt => opt.MapFrom(src => src.iIdPermiso))
+				.ForMember(dest => dest.Nombre, opt => opt.MapFrom(src => src.sNombre))
+				.ForMember(dest => dest.Descripcion, opt => opt.MapFrom(src => src.sDescripcion));
+		}
+	}
+}
