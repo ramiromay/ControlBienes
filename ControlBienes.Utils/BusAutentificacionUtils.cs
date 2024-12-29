9a394734afb6cd0a83facdda35e96757e8f74bee
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 17:50:30 -0600

    Agrega utilidades de conversion de errores a formato texto

---
+++ ControlBienes.Utils/BusAutentificacionUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace ControlBienes.Utils
+{
+    public class BusAutentificacionUtils
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+
+    }
+}
---
+++ ControlBienes.Utils/BusConvertirErrors.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.Results;
+
+namespace ControlBienes.Utils
+{
+    public class BusConvertirErrors
+    {
+        public static string UFormatearTexto(List<ValidationFailure> errores)
+        {
+            string resultado = "";
+            foreach (var error in errores)
+            {
+                resultado += $"{error.ErrorMessage},";
+            }
+
+            if (errores.Count > 0)
+                resultado = URemoverComillaFinal(resultado);
+
+            return resultado;
+        }
+
+        public static string URemoverComillaFinal(string errores)
+        {
+            if (string.IsNullOrEmpty(errores)) return string.Empty;
+            string resultado = errores;
+            string ultimooCaracter = errores.Substring(errores.Length - 1);
+
+            if (ultimooCaracter.Equals(",", StringComparison.OrdinalIgnoreCase))
+                resultado = resultado.Remove(resultado.Length - 1);
+
+            return resultado;
+        }
+
+    }
+}
