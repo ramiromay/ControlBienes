00cebe7c5313e7984afd6b5c41540401ff0c869b
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 17:47:02 -0600

    Agrega implementacion de funcionalidades de lectura para el listado de personas

---
+++ ControlBienes.Business/Features/Seguridad/Persona/BusPersona.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using ControlBienes.Business.Contrats.General;
+using ControlBienes.Business.Contrats.Seguridad;
+using ControlBienes.Business.Exceptions;
+using ControlBienes.Business.Features.Catalogos.Color;
+using ControlBienes.Business.Genericos;
+using ControlBienes.Data.Contrats.Catalogos;
+using ControlBienes.Data.Contrats.Seguridad;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.Constants;
+using ControlBienes.Entities.General.Periodo;
+using ControlBienes.Entities.Seguridad.Persona.Persona;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace ControlBienes.Business.Features.Seguridad.Persona
+{
+    public class BusPersona : IBusPersona
+    {
+        private readonly IDatPersona _repositorio;
+        private readonly ILogger<BusPersona> _logger;
+        private readonly IMapper _mapper;
+        const EnumCodigoOperacion _code = EnumCodigoOperacion.CodeOkPersona;
+        const EnumCodigoOperacion _codeError = EnumCodigoOperacion.CodeErrorPersona;
+
+        public BusPersona(IDatPersona repositorio, IMapper mapper, ILogger<BusPersona> logger)
+        {
+            _repositorio = repositorio;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<EntityResponse<IEnumerable<EntPersonaResponse>>> BObtenerTodasPersonasAsync()
+        {
+            var nombreMetodo = nameof(BObtenerTodasPersonasAsync);
+            var resultado = new EntityResponse<IEnumerable<EntPersonaResponse>>();
+
+            _logger.LogInformation($"{(long)_code}: Inicia la operacion para consultar todos las personas");
+            try
+            {
+                var entidades = await _repositorio.DObtenerTodosAsync();
+                resultado.Result = _mapper.Map<IEnumerable<EntPersonaResponse>>(entidades);
+                resultado.StatusCode = HttpStatusCode.OK;
+                resultado.Message = EntMensajeConstant.OK;
+                resultado.Code = _code;
+            }
+            catch (Exception ex)
+            {
+                resultado = BusExceptionHandler.Handle<IEnumerable<EntPersonaResponse>>(
+                    _logger,
+                    ex,
+                    _codeError,
+                    nombreMetodo,
+                    "Ocurrio un error al intentar consultar todos las personas"
+                );
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ ControlBienes.Business/Features/Seguridad/Persona/BusPersonaMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ControlBienes.Entities.Seguridad.Persona;
+using ControlBienes.Entities.Seguridad.Persona.Persona;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Seguridad.Persona
+{
+    public class BusPersonaMapping : Profile
+    {
+        public BusPersonaMapping()
+        {
+            CreateMap<EntPersona, EntPersonaResponse>()
+               .ForMember(dest => dest.IdPersona, opt => opt.MapFrom(src => src.iIdPersona))
+               .ForMember(dest => dest.NombreCompleto, opt => opt.MapFrom(src => $"{src.sNombres} {src.sPrimerNombre} {src.sSegundoNombre}".Trim()));
+
+        }
+    }
+}
