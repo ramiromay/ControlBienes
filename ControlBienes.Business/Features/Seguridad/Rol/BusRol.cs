8fa653a6d96dc7014b4abb9b05a2c1f9dae06503
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 17:46:33 -0600

    Agrega implementacion de funcionalidades de lectura del listado de roles

---
+++ ControlBienes.Business/Features/Seguridad/Rol/BusRol.cs
@@ -0,0 +1,56 @@
+﻿using ControlBienes.Business.Contrats.Seguridad;
+using ControlBienes.Business.Exceptions;
+using ControlBienes.Business.Genericos;
+using ControlBienes.Data.Contrats.Seguridad;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.Constants;
+using ControlBienes.Entities.Seguridad.Rol;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Seguridad.Rol
+{
+	public class BusRol : IBusRol
+	{
+		private readonly IDatRol _repositorio;
+		private readonly ILogger<BusRol> _logger;
+		const EnumCodigoOperacion _code = EnumCodigoOperacion.CodeOkRol;
+		const EnumCodigoOperacion _codeError = EnumCodigoOperacion.CodeErrorRol;
+
+		public BusRol(IDatRol repositorio, ILogger<BusRol> logger)
+		{
+			_repositorio = repositorio;
+			_logger = logger;
+		}
+
+		public async Task<EntityResponse<IEnumerable<EntRolResponse>>> BObtenerTodosRolesAsync()
+		{
+			var nombreMetodo = nameof(BObtenerTodosRolesAsync);
+			var resultado = new EntityResponse<IEnumerable<EntRolResponse>>();
+
+			_logger.LogInformation($"{(long)_code}: Inicia la operacion para consultar todos los roles");
+			try
+			{
+				var resultDto = await _repositorio.DObtenerProyeccionListaAsync<EntRolResponse>();
+				resultado.Success(resultDto, _code);
+			}
+			catch (Exception ex)
+			{
+				resultado = BusExceptionHandler.Handle<IEnumerable<EntRolResponse>>(
+					_logger,
+					ex,
+					_codeError,
+					nombreMetodo,
+					"Ocurrio un error al intentar consultar todos los roles"
+				);
+			}
+
+			return resultado;
+		}
+	}
+}
---
+++ ControlBienes.Business/Features/Seguridad/Rol/BusRolMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.Seguridad.Rol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Seguridad.Rol
+{
+	public class BusRolMapping : Profile
+	{
+		public BusRolMapping()
+		{
+			CreateMap<EntRol, EntRolResponse>()
+				.ForMember(dest => dest.IdRol, opt => opt.MapFrom(src => src.Id))
+				.ForMember(dest => dest.Nombre, opt => opt.MapFrom(src => src.Name))
+				.ForMember(dest => dest.Descripcion, opt => opt.MapFrom(src => src.sDescripcion));
+		}
+	}
+}
