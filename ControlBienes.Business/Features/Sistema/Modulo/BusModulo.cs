7bf71a5e37ed244b5fd742b2a25ee1665ae3e4ab
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 17:43:55 -0600

    Agrega implementacion de funcionalidades de lectura para el listado de modulos del sistema

---
+++ ControlBienes.Business/Features/Sistema/Modulo/BusModulo.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using ControlBienes.Business.Contrats.Seguridad;
+using ControlBienes.Business.Contrats.Sistema;
+using ControlBienes.Business.Exceptions;
+using ControlBienes.Business.Features.Catalogos.Color;
+using ControlBienes.Business.Genericos;
+using ControlBienes.Data.Contrats.Catalogos;
+using ControlBienes.Data.Contrats.Seguridad;
+using ControlBienes.Data.Contrats.Sistema;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.Constants;
+using ControlBienes.Entities.Sistema.Modulo.Modulo;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Sistema.Modulo
+{
+    public class BusModulo : IBusModulo
+    {
+        private readonly IDatModulo _repositorio;
+		private readonly IDatUsuarioPermiso _repositorioUsuarioPermiso;
+		private readonly IBusIdentityAccess _servicioIdentityAccess;
+		private readonly ILogger<BusModulo> _logger;
+        private readonly IMapper _mapper;
+        const EnumCodigoOperacion _code = EnumCodigoOperacion.CodeOkColor;
+        const EnumCodigoOperacion _codeError = EnumCodigoOperacion.CodeErrorColor;
+
+        public BusModulo(IDatModulo repositorio, 
+            IMapper mapper, 
+            ILogger<BusModulo> logger,
+            IBusIdentityAccess servicioIdentityAccess,
+			IDatUsuarioPermiso repositorioUsuarioPermiso)
+        {
+            _repositorio = repositorio;
+            _repositorioUsuarioPermiso = repositorioUsuarioPermiso;
+            _servicioIdentityAccess = servicioIdentityAccess;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<EntityResponse<IEnumerable<EntModuloResponse>>> BObtenerTodosModulos()
+        {
+            var nombreMetodo = nameof(BObtenerTodosModulos);
+            var resultado = new EntityResponse<IEnumerable<EntModuloResponse>>();
+
+            _logger.LogInformation($"{(long)_code}: Inicia la operacion para consultar todos los modulos");
+            try
+            {
+				var idUsuario = _servicioIdentityAccess.BObtenerIdUsuario();
+				var permisosUsuario = await _repositorioUsuarioPermiso.DObtenerTodosAsync(null, e => e.iIdUsuario == idUsuario);
+				var permisosIds = permisosUsuario.Select(p => p.iIdPermiso).ToList();
+				var modulos = await _repositorio.DObtenerTodosAsync(null, e => permisosIds.Contains(e.iIdPermiso));
+                var result = _mapper.Map<IEnumerable<EntModuloResponse>>(modulos);
+                resultado.Success(result, _code);
+            }
+            catch (Exception ex)
+            {
+                resultado = BusExceptionHandler.Handle<IEnumerable<EntModuloResponse>>(
+                    _logger,
+                    ex,
+                    _codeError,
+                    nombreMetodo,
+                    "Ocurrio un error al intentar consultar todos los modulos"
+                );
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ ControlBienes.Business/Features/Sistema/Modulo/BusModuloMapping.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+
+using ControlBienes.Entities.Sistema.Modulo;
+using ControlBienes.Entities.Sistema.Modulo.Modulo;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Sistema.Modulo
+{
+    public class BusModuloMapping : Profile
+    {
+        
+        public BusModuloMapping()
+        {
+            CreateMap<EntModulo, EntModuloResponse>()
+              .ForMember(dest => dest.IdModulo, opt => opt.MapFrom(src => src.iIdModulo))
+              .ForMember(dest => dest.Nombre, opt => opt.MapFrom(src => src.sNombre))
+              .ForMember(dest => dest.Abreviacion, opt => opt.MapFrom(src => src.sAbreviacion))
+              .ForMember(dest => dest.Descripcion, opt => opt.MapFrom(src => src.sDescripcion));
+        }
+    }
+}
