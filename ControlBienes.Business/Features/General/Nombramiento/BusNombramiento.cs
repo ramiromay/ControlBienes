f7271a6810c59ce6072a54ea80009bd1ef9dfcbd
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 17:12:36 -0600

    Agrega implementacion de funcionalidades de lectura para los nombramientos

---
+++ ControlBienes.Business/Features/General/Nombramiento/BusNombramiento.cs
@@ -0,0 +1,58 @@
+﻿using ControlBienes.Business.Contrats.General;
+using ControlBienes.Business.Exceptions;
+using ControlBienes.Business.Genericos;
+using ControlBienes.Data.Contrats.General;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.Constants;
+using ControlBienes.Entities.General.Nacionalidad;
+using ControlBienes.Entities.General.Nombramiento;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.General.Nombramiento
+{
+	public class BusNombramiento : IBusNombramiento
+	{
+		private readonly IDatNombramiento _repositorio;
+		private readonly ILogger<BusNombramiento> _logger;
+		const EnumCodigoOperacion _code = EnumCodigoOperacion.CodeOkNombramiento;
+		const EnumCodigoOperacion _codeError = EnumCodigoOperacion.CodeErrorNombramiento;
+
+
+		public BusNombramiento(IDatNombramiento repositorio, ILogger<BusNombramiento> logger)
+		{
+			_repositorio = repositorio;
+			_logger = logger;
+		}
+
+		public async Task<EntityResponse<IEnumerable<EntNombramientoResponse>>> BObtenerTodosNombramientos()
+		{
+			var nombreMetodo = nameof(BObtenerTodosNombramientos);
+			var resultado = new EntityResponse<IEnumerable<EntNombramientoResponse>>();
+
+			_logger.LogInformation($"{(long)_code}: Inicia la operacion para consultar todos las nombramientos");
+			try
+			{
+				var resultDto = await _repositorio.DObtenerProyeccionListaAsync<EntNombramientoResponse>();
+				resultado.Success(resultDto, _code);
+			}
+			catch (Exception ex)
+			{
+				resultado = BusExceptionHandler.Handle<IEnumerable<EntNombramientoResponse>>(
+					_logger,
+					ex,
+					_codeError,
+					nombreMetodo,
+					"Ocurrio un error al intentar consultar todos los nombramientos"
+				);
+			}
+
+			return resultado;
+		}
+	}
+}
---
+++ ControlBienes.Business/Features/General/Nombramiento/BusNombramientoMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.General.Nombramiento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.General.Nombramiento
+{
+	public class BusNombramientoMapping : Profile
+	{
+		public BusNombramientoMapping()
+		{
+			CreateMap<EntNombramiento, EntNombramientoResponse>()
+				.ForMember(dest => dest.IdNombramiento, opt => opt.MapFrom(src => src.iIdNombramiento))
+				.ForMember(dest => dest.Nombre, opt => opt.MapFrom(src => src.sNombre));
+		}
+	}
+}
