e80f4809188ae252746939a8fa9f0b27f92791ae
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 18:15:26 -0600

    Agrega el mapping para los tipos de tramites

---
+++ ControlBienes.Business/Features/Patrimonio/MotivoTramite/BusMotivoTramiteMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.Patrimonio.MotivoTramite;
+using ControlBienes.Entities.Patrimonio.TipoTramite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Patrimonio.MotivoTramite
+{
+    public class BusMotivoTramiteMapping : Profile
+    {
+        public BusMotivoTramiteMapping()
+        {
+            CreateMap<EntMotivoTramite, EntMotivoTramiteResponse>()
+              .ForMember(dest => dest.IdMotivoTramite, opt => opt.MapFrom(src => src.iIdMotivoTramite))
+              .ForMember(dest => dest.Nombre, opt => opt.MapFrom(src => src.sNombre))
+              .ForMember(dest => dest.IdTipoTramite, opt => opt.MapFrom(src => src.iIdTipoTramite));
+        }
+    }
+}
