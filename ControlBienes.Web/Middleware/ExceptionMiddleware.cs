5a9aa2d3fb9bd7a08cb2db762ddacf4798883b6a
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 18:05:58 -0600

    Agrega el middleware

---
+++ ControlBienes.Web/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using ControlBienes.Business.Genericos;
+using ControlBienes.Entities.Constants;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ControlBienes.Web.Middleware
+{
+    public class ExceptionMiddleware
+    {
+
+        private readonly RequestDelegate _requestDelegate;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionMiddleware> logger)
+        {
+            _requestDelegate = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                
+                await _requestDelegate(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("Error en la peticion");
+                _logger.LogError(ex, ex.Message);
+                EntityResponse<string> resultado = new()
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    Message = $"Ocurrio un error interno: ",
+                    HasError = true,
+                    Code = EnumCodigoOperacion.CodeErrorColor
+                };
+
+                context.Response.ContentType = "application/json";
+
+                //switch (ex)
+                //{
+                //    case ValidationException validationException:
+                //        statusCode = (int)HttpStatusCode.BadRequest;
+                //        var validationJson = JsonConvert.SerializeObject(validationException.Errors);
+                //        result = JsonConvert.SerializeObject(new CodeErrorException(statusCode, ex.Message, validationJson));
+                //        break;
+                //    default:
+                //        break;
+                //}
+
+                resultado.Result = JsonConvert.SerializeObject(resultado);
+                await context.Response.WriteAsync(resultado.Result);
+
+            }
+        }
+
+    }
+}
