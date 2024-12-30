820100fa51982ed762f1a5d0c74d07fd3a323c82
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 18:29:04 -0600

    Agregar configuracion de mapeo para tipos de tramite

---
+++ ControlBienes.Business/Features/Patrimonio/TipoTramite/BusTipoTramiteMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.Patrimonio.TipoTramite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Features.Patrimonio.TipoTramite
+{
+    public class BusTipoTramiteMapping : Profile
+    {
+        public BusTipoTramiteMapping()
+        {
+            CreateMap<EntTipoTramite, EntTipoTramiteResponse>()
+              .ForMember(dest => dest.IdTipoTramite, opt => opt.MapFrom(src => src.iIdTipoTramite))
+              .ForMember(dest => dest.Nombre, opt => opt.MapFrom(src => src.sNombre))
+              .ForMember(dest => dest.IdSubModulo, opt => opt.MapFrom(src => src.iIdSubModulo));
+        }
+    }
+}
