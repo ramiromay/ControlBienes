872e87eedca73a2156b0480aa3bc160d39997816
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 18:27:14 -0600

    Agrega interfaces para tipos de tramites

---
+++ ControlBienes.Business/Contrats/Patrimonio/IBusTipoTramite.cs
@@ -0,0 +1,16 @@
+﻿using ControlBienes.Business.Genericos;
+using ControlBienes.Entities.Patrimonio.TipoTramite;
+using ControlBienes.Entities.Sistema.SubModulo.SubModulo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Contrats.Patrimonio
+{
+    public interface IBusTipoTramite
+    {
+        Task<EntityResponse<IEnumerable<EntTipoTramiteResponse>>> BObtenerTodosTiposTramites();
+    }
+}
