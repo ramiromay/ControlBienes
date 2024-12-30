d7231e86ccb5b276e5796b11f81f842d25ced189
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 18:03:30 -0600

    Agrega los controladores para consulta los datos generales

---
+++ ControlBienes.Web/Controllers/General/GeneralController.cs
@@ -0,0 +1,44 @@
+﻿using ControlBienes.Business.Contrats.General;
+using ControlBienes.Business.Genericos;
+using ControlBienes.Entities.General.Nacionalidad;
+using ControlBienes.Entities.General.Nombramiento;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlBienes.Web.Controllers.General
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class GeneralController : ControllerBase
+	{
+		private readonly IBusNacionalidad _servicioNacionalidad;
+		private readonly IBusNombramiento _servicioNombramiento;
+
+		public GeneralController(IBusNacionalidad servicioNacionalidad, IBusNombramiento servicioNombramiento)
+		{
+			_servicioNacionalidad = servicioNacionalidad;
+			_servicioNombramiento = servicioNombramiento;
+		}
+
+		[HttpGet("Nacionalidad")]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EntityResponse<EntNacionalidadResponse>))]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(EntityResponse<EntNacionalidadResponse>))]
+		[ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(EntityResponse<EntNacionalidadResponse>))]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(EntityResponse<EntNacionalidadResponse>))]
+		public async Task<ActionResult<EntityResponse<IEnumerable<EntNacionalidadResponse>>>> CObtenerTodasNacionalidades()
+		{
+			var response = await _servicioNacionalidad.BObtenerTodadNacionalidades();
+			return StatusCode((int)response.StatusCode, response);
+		}
+
+		[HttpGet("Nombramiento")]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EntityResponse<EntNombramientoResponse>))]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(EntityResponse<EntNombramientoResponse>))]
+		[ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(EntityResponse<EntNombramientoResponse>))]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(EntityResponse<EntNombramientoResponse>))]
+		public async Task<ActionResult<EntityResponse<IEnumerable<EntNombramientoResponse>>>> CObtenerTodosNombramientoes()
+		{
+			var response = await _servicioNombramiento.BObtenerTodosNombramientos();
+			return StatusCode((int)response.StatusCode, response);
+		}
+	}
+}
---
+++ ControlBienes.Web/Controllers/General/PeriodoController.cs
@@ -0,0 +1,30 @@
+﻿using ControlBienes.Business.Contrats.General;
+using ControlBienes.Business.Genericos;
+using ControlBienes.Entities.Catalogos.Color;
+using ControlBienes.Entities.General.Periodo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlBienes.Web.Controllers.General
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class PeriodoController : ControllerBase
+	{
+		private readonly IBusPeriodo _servicio;
+		public PeriodoController(IBusPeriodo servicio)
+		{
+			_servicio = servicio;
+		}
+
+		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EntityResponse<EntPeriodoResponse>))]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(EntityResponse<EntPeriodoResponse>))]
+		[ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(EntityResponse<EntPeriodoResponse>))]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(EntityResponse<EntPeriodoResponse>))]
+		public async Task<EntityResponse<IEnumerable<EntPeriodoResponse>>> CObtenerTodosPeriodos()
+		{
+
+			return await _servicio.BObtenerPeriodosAsync();
+		}
+	}
+}
