9d5f292b89230e8c2ba7180d70a48c4a2e9c8c88
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 18:11:54 -0600

    Agrega implemnetacion del repositorio motivo del tramite

---
+++ ControlBienes.Data/Repository/Patrimonio/DatMotivoTramite.cs
@@ -0,0 +1,19 @@
+﻿using ControlBienes.Data.Contrats.Patrimonio;
+using ControlBienes.Data.Persistence;
+using ControlBienes.Entities.Patrimonio.MotivoTramite;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Data.Repository.Patrimonio
+{
+    public class DatMotivoTramite : Dat<EntMotivoTramite>, IDatMotivoTramite
+    {
+        public DatMotivoTramite(AppDbContext context, ILogger<Dat<EntMotivoTramite>> logger) : base(context, logger)
+        {
+        }
+    }
+}
