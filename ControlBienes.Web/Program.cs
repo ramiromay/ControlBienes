7823cfe4d3e3cb0d14f9457f24bab06792914051
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 18:06:21 -0600

    Agrega el proyecto base web

--- ControlBienes.Web/Program.cs
+++ ControlBienes.Web/Program.cs
@@ -1,11 +1,32 @@
+using ControlBienes.Business;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configuracion de servicios
+builder.Services.BRegistrarServiciosApp(builder, builder.Configuration);
+
+
+
+
+// Configuracion de CORS
+builder.Services.AddCors(options =>
+{
+     options.AddPolicy("CorsPolicy", policy =>
+     {
+          policy.WithOrigins("http://localhost:5173", "http://localhost:5174") // Dominio del cliente permitido
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials(); // Permite el uso de credenciales
+     });
+});
 
 var app = builder.Build();
 
@@ -18,7 +39,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Configuracion de Cookie
+// app.UseCookiePolicy();
+
+// Configuracion uso de Autentificacion con JWT
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
