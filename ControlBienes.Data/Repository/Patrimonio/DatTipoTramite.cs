5bb22b79dd9961f0e26b63e35bf3045d65b24ee8
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 18:28:04 -0600

    Agregar implementacion de acceso a datos para tipos de trámite

---
+++ ControlBienes.Data/Repository/Patrimonio/DatTipoTramite.cs
@@ -0,0 +1,19 @@
+﻿using ControlBienes.Data.Contrats.Patrimonio;
+using ControlBienes.Data.Persistence;
+using ControlBienes.Entities.Patrimonio.TipoTramite;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Data.Repository.Patrimonio
+{
+    public class DatTipoTramite : Dat<EntTipoTramite>, IDatTipoTramite
+    {
+        public DatTipoTramite(AppDbContext context, ILogger<Dat<EntTipoTramite>> logger) : base(context, logger)
+        {
+        }
+    }
+}
