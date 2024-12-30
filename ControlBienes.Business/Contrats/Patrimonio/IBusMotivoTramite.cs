1fc8cfd96564086811e9daf2922521489dd19667
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2024-12-29 18:13:41 -0600

    Agrega el contrato de tipos de tramites para la capa de negocios

---
+++ ControlBienes.Business/Contrats/Patrimonio/IBusMotivoTramite.cs
@@ -0,0 +1,16 @@
+﻿using ControlBienes.Business.Genericos;
+using ControlBienes.Entities.Patrimonio.MotivoTramite;
+using ControlBienes.Entities.Sistema.SubModulo.SubModulo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBienes.Business.Contrats.Patrimonio
+{
+    public interface IBusMotivoTramite
+    {
+        Task<EntityResponse<IEnumerable<EntMotivoTramiteResponse>>> BObtenerTodosMotivosTramites();
+    }
+}
