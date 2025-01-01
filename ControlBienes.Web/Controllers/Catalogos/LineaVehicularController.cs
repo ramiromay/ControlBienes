6469c786a1badb0a77adaf4f296a4a614e4cb2e4
Ramiro May <123800454+ramiromay@users.noreply.github.com>
2025-01-01 15:51:50 -0600

    Cambio la funcionalidad para permitir obtener las lineas vehiculares  activas e inactivas

--- ControlBienes.Web/Controllers/Catalogos/LineaVehicularController.cs
+++ ControlBienes.Web/Controllers/Catalogos/LineaVehicularController.cs
@@ -22,10 +22,10 @@ namespace ControlBienes.Web.Controllers.Catalogos
 		[ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(EntityResponse<EntLineaVehicularResponse>))]
 		[ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(EntityResponse<EntLineaVehicularResponse>))]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(EntityResponse<EntLineaVehicularResponse>))]
-		public async Task<EntityResponse<IEnumerable<EntLineaVehicularResponse>>> CObtenerTodosColores()
+		public async Task<EntityResponse<IEnumerable<EntLineaVehicularResponse>>> CObtenerTodosColores([FromQuery] bool? activo)
 		{
 
-			return await _servicio.BObtenerTodosAsync();
+			return await _servicio.BObtenerTodosAsync(activo);
 		}
 
 		[HttpGet("{id}")]
